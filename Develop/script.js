867d39188ba3ef4e064cf3e147d205c158639910
Nathan Szurek <nathan.szurek@yahoo.com>
2020-06-11 22:29:25 -0500

    Linked functions and created event listener.

--- Develop/script.js
+++ Develop/script.js
@@ -18,104 +18,91 @@ generateBtn.addEventListener("click", writePassword);
 // ON BUTTON CLICK
 // Need a function to initiate the password generation code.
 
+
+function generatePassword() {
+
 // PROMPTED TO CHOOSE PASSOWRD LENGTH
 // Prompt to allow a user to input a number from 8-128.
 // If else (if number is less than 8 or greater than 128, alert "Password must be between 8 and 128 characters.").
 // Else store number in VAR then continue with prompts.
 
-var passLength = prompt("Enter a password length from 8 - 128");
 
-if(passLength < 8 || passLength > 128) {
-  alert("Password length must be between 8 and 128!");
+  var passLength = prompt("Enter a password length from 8 - 128");
 
-}
-else {
-
-}
-
-if(passLength > 7 && passLength < 129) {
-
-// PROMPTED TO INCLUDE LOWERCASE LETTERS
-// Prompt to allow a user to confirm the use of lowercase letters.
-// If else (if true then add lowercase letters to array options).
-// Else skip and continue with prompts.
-
-  var lowerCase = confirm("Use lowercase letters?")
-
-  if(lowerCase) {
-      arr.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-  }
-  else {
-    
-  }
-
-// PROMPTED TO INCLUDE UPPERCASE LETTERS
-// Prompt to allow a user to confirm the use of uppercase letters.
-// If else (if true then add uppercase letters to array options).
-// Else skip and continue with prompts.
-  
-  var upperCase = confirm("Use uppercase letters?")
-  
-  if(upperCase) {
-      arr.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
-  }    
-  else {
+  if(passLength < 8 || passLength > 128) {
+    alert("Password length must be between 8 and 128!");
   
   }
-
-// PROMPTED TO INCLUDE NUMBERS
-// Prompt to allow a user to confirm the use of numbers.
-// If else (if true then add numbers to array options).
-// Else skip and continue with prompts.
-
-  var number = confirm("Use numbers?")
-
-  if(number) {
-    arr.push(0,1,2,3,4,5,6,7,8,9)
-  }    
-  else {
-
-  }
-
-// PROMPTED TO INCLUDE SPECIAL CHARACTERS
-// Prompt to allow a user to confirm the use of special characters.
-// If else (if true then add special characters to array options).
-// Else skip and continue to validation.
-
-  var special = confirm("Use special characters?")
-    
-  if(special) {
-      arr.push("!","#","$","%","&","'","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","}","~")
-  }    
-  else {
-
-  }
+  else if(passLength > 7 && passLength < 129) {
   
-  console.log(arr)
-
-// INPUTS ARE VALIDATED TO ENSURE THAT AT LEAST ONE OPTION IS CHOSEN
-// If else (if no options are selected, alert "please choose at least one" then reload page).
-// Else continue to generate and write password.
+  // PROMPTED TO INCLUDE LOWERCASE LETTERS
+  // Prompt to allow a user to confirm the use of lowercase letters.
+  // If else (if true then add lowercase letters to array options).
+  // Else skip and continue with prompts.
   
-  if(!lowerCase && !upperCase && !number && !special) {
-    alert("You must choose at least one option!");
-    window.location = window.location;
-  }
-  else {
-
-    for(i=0 ; i < passLength ; i++) {
-      pass.push(arr[Math.floor(Math.random()*arr.length)]);
+    var lowerCase = confirm("Use lowercase letters?")
+  
+    if(lowerCase) {
+        arr.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
     }
-    console.log(pass);
-    alert(pass);
-    document.getElementById("print").innerHTML = pass;
+  
+  // PROMPTED TO INCLUDE UPPERCASE LETTERS
+  // Prompt to allow a user to confirm the use of uppercase letters.
+  // If else (if true then add uppercase letters to array options).
+  // Else skip and continue with prompts.
+    
+    var upperCase = confirm("Use uppercase letters?")
+    
+    if(upperCase) {
+        arr.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+    }    
+  
+  // PROMPTED TO INCLUDE NUMBERS
+  // Prompt to allow a user to confirm the use of numbers.
+  // If else (if true then add numbers to array options).
+  // Else skip and continue with prompts.
+  
+    var number = confirm("Use numbers?")
+  
+    if(number) {
+      arr.push(0,1,2,3,4,5,6,7,8,9)
+    }    
+  
+  // PROMPTED TO INCLUDE SPECIAL CHARACTERS
+  // Prompt to allow a user to confirm the use of special characters.
+  // If else (if true then add special characters to array options).
+  // Else skip and continue to validation.
+  
+    var special = confirm("Use special characters?")
+      
+    if(special) {
+        arr.push("!","#","$","%","&","'","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","}","~")
+    }    
+    
+    console.log(arr)
+  
+  // INPUTS ARE VALIDATED TO ENSURE THAT AT LEAST ONE OPTION IS CHOSEN
+  // If else (if no options are selected, alert "please choose at least one" then reload page).
+  // Else continue to generate and write password.
+    
+    if(!lowerCase && !upperCase && !number && !special) {
+      alert("You must choose at least one option!");
+      window.location = window.location;
+    }
+    else {
+  
+      for(i=0 ; i < passLength ; i++) {
+        pass.push(arr[Math.floor(Math.random()*arr.length)]);
+        
+      }
 
+      return pass.join("");
+
+    }
   }
-
-
-}
-else {
-  window.location = window.location;
+  else {
+    alert("You must type a number!");
+  }
 }
 
 // PASSWORD IS GENERATED USING THE PRESELECTED CRITERIA
